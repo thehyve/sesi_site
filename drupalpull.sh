cb45599974d406da22e30a2cba8c29e990247a9a
Riza Nugraha <rnugraha@gmail.com>
2014-10-10 15:34:13 +0000

    Disabling captcha and enabling contact form feature

--- drupalpull.sh
+++ drupalpull.sh
@@ -48,8 +48,8 @@ sudo drush --yes en og og_ui og_context
 drush pm-enable sesi_communities_and_files
 
 #install captcha
-drush --yes dl captcha
-drush --yes en image_captcha
+#drush --yes dl captcha
+#drush --yes en image_captcha
 
 #backup first
 #drush archive-dump /tmp/micasitebk
@@ -84,6 +84,8 @@ drush --yes features-revert sesi_autologout
 
 # Enable Contact Form
 drush --yes pm-enable contact
+drush --yes pm-enable sesi_contact_form
+drush --yes features-revert sesi_contact_form
 
 # ////////////////////////// 
 # Update
