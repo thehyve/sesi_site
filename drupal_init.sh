69205ca66829573d889aca08c9f67a7f05be404a
Riza Nugraha <rnugraha@gmail.com>
2014-10-10 15:31:41 +0000

    Add script mainly configures the initial contents for Mica Sesi project.

---
+++ drupal_init.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# drupal_init.sh
+# --------------
+# This script mainly configures the initial contents for Mica Sesi project.
+# 
+
+set -e
+
+echo ' (                              (                                        '
+echo ' )\ )                      (    )\ )           )                         '
+echo '(()/(  (     (           ) )\  (()/(     (  ( /(                         '
+echo ' /(_)) )(   ))\ `  )  ( /(((_)  /(_))(   )\ )\())                        '
+echo '(_))_ (()\ /((_)/(/(  )(_))_   (_))  )\ |(_|_))/                         '
+echo '|   \ ((_|_))(((_)_\((_)_| |  |_ _|_(_/((_) |_                           '
+echo '| |) | ,_| || | ,_ \) _` | |   | || , \)) |  _|                          ' 
+echo '|___/|_|  \_,_| .__/\__,_|_|  |___|_||_||_|\__|                          ' 
+echo '               |_|                                                       '
+
+# --------------- #
+# Get drupal root #
+# --------------- #
+DRUPAL_ROOT=`drush status | grep 'Drupal root' | sed 's/.*:[ ]*//' | sed 's/ *$//'`
+echo $DRUPAL_ROOT
+
+# ------------------------ #
+# Enable Menu Links Config #
+# ------------------------ #
+drush --yes pm-enable sesi_menu_links
+drush --yes features-revert sesi_menu_links
+
+# -------------------------------------------------- #
+# Display list of features to check status manually. #
+# -------------------------------------------------- #
+drush features
