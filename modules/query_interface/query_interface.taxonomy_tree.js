15644ba2568abc8f80c949440d826dacac80fb27
Robert Horlings <robert@isdat.nl>
2014-10-20 09:40:07 +0000

    Initial addition of taxonomy tree structure

---
+++ modules/query_interface/query_interface.taxonomy_tree.js
@@ -0,0 +1,16 @@
+// Using the closure to map jQuery to $.
+(function ($) {
+    Drupal.behaviors.query_interface_taxonomy_tree = {
+        attach: function (context, settings) {
+            // init the tree
+                $('#taxonomy_tree').aciTree({
+                    ajax: {
+                        url: '/mica/sites/all/modules/query_interface/checkbox.json'
+                    },
+                    checkbox: true
+                });
+        }
+    };
+
+}(jQuery)); 
+
