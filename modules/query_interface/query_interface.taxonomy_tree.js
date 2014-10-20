1f1c79f29492637da0afdc5f08611f992b3fa539
Robert Horlings <robert@isdat.nl>
2014-10-20 10:22:34 +0000

    Bugfix in javascripts in other screens than the query_interface

--- modules/query_interface/query_interface.taxonomy_tree.js
+++ modules/query_interface/query_interface.taxonomy_tree.js
@@ -3,12 +3,12 @@
     Drupal.behaviors.query_interface_taxonomy_tree = {
         attach: function (context, settings) {
             // init the tree
-                $('#taxonomy_tree').aciTree({
-                    ajax: {
-                        url: '/mica/sites/all/modules/query_interface/checkbox.json'
-                    },
-                    checkbox: true
-                });
+            jQuery('#taxonomy_tree').aciTree({
+                ajax: {
+                    url: '/mica/sites/all/modules/query_interface/checkbox.json'
+                },
+                checkbox: true
+            });
         }
     };
 
