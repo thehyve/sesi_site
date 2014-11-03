618b44711c831d06b4e16122d220d1b24e39f0e6
Robert Horlings <robert@isdat.nl>
2014-11-03 10:53:56 +0000

    SESIDEV-2 Added select/deselect all button to taxonomy tree

--- modules/query_interface/query_interface.taxonomy_tree.js
+++ modules/query_interface/query_interface.taxonomy_tree.js
@@ -1,9 +1,17 @@
-// Using the closure to map jQuery to $.
+// Initialize namespace
+if( typeof Sesi == 'undefined' ) 
+    Sesi = {};
+
+if( typeof Sesi.QueryInterface == 'undefined' ) 
+    Sesi.QueryInterface = {};
+
 (function ($) {
+
+    // Make sure that the taxonomy tree is loaded
     Drupal.behaviors.query_interface_taxonomy_tree = {
         attach: function (context, settings) {
             // init the tree
-            var tree = jQuery('#taxonomy_tree');
+            var tree = $('#taxonomy_tree');
             if( tree.length > 0 ) {
                 tree.aciTree({
                     ajax: {
@@ -21,6 +29,31 @@
             }
         }
     };
+
+    // Initialize TaxonomyTree namespace
+    if( typeof Sesi.QueryInterface.TaxonomyTree == 'undefined' )
+        Sesi.QueryInterface.TaxonomyTree = {};
+
+    // (de)select all elements in the taxonomy tree    
+    // If no argument is given, all elements are selected
+    Sesi.QueryInterface.TaxonomyTree.selectAll = function(checked) {
+        if( typeof checked == 'undefined' )
+            checked = true;
+
+        // Retrieve a reference to the tree api
+        var taxonomyTree = $( Sesi.QueryInterface.selectors.taxonomyTree );
+        var treeApi = taxonomyTree.aciTree('api');
+
+        // Loop through all items and (de)select all items
+        var allItems = treeApi.children( null, true );
+
+        for( i = 0; i < allItems.length; i++ ) {
+            if( checked ) 
+                treeApi.check( allItems.eq(i) );
+            else
+                treeApi.uncheck( allItems.eq(i) );
+        }
+    }
 
 }(jQuery)); 
 
