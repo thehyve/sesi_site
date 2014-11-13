4cb03197e5036f10c1209a3d03bc3da5570fbbe1
root <root@localhost.localdomain>
2014-11-13 15:50:18 +0000

    disabled locale (not i18n required) and enabled update of drupal CORE in init

--- drupalpull.sh
+++ drupalpull.sh
@@ -31,7 +31,7 @@ drush --yes pm-disable bartik
 # Enable some modules that must be enabled.
 drush --yes pm-enable features
 drush --yes pm-enable strongarm
-drush --yes pm-enable locale
+drush --yes dis locale
 
 # Install and enable Features Extra module
 drush --yes dl features_extra
