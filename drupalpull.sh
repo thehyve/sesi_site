f5304cf87d63e57a63a8270e6f9d7310dd5da8e5
Jan Kanis <jan.code@jankanis.nl>
2014-11-25 16:10:17 +0000

    Give administrators access to queries

--- drupalpull.sh
+++ drupalpull.sh
@@ -132,9 +132,10 @@ ensure_feat sesi_dataset_access_form
 ensure_feat sesi_vocabulary
 
 #query
-isdisabled query_interface && drush --yes en query_interface
-isdisabled query_subscription && drush --yes en query_subscription
+ensure_mod query_interface
+ensure_mod query_subscription
 ensure_feat sesi_my_queries_screen
+ensure_mod data_access_form
 
 #ontologies and vocabularies
 ensure_feat sesi_variable_ontologies
