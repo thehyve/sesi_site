f5413ae1da5a1d4f10004a30a2a7a954065b6d1b
Riza Nugraha <rnugraha@gmail.com>
2014-11-28 11:04:01 +0000

    To give the options to admin to show/hide title of a page.

--- themes/mica_sesi_subtheme/template.php
+++ themes/mica_sesi_subtheme/template.php
@@ -7,3 +7,22 @@
 function mica_subtheme_bootstrap_based_theme() {
   return array('mica_subtheme' => TRUE);
 }
+
+/**
+ * Check if we need to show page title or not.
+ * 
+ * Implements hook_process_page().
+ */
+function mica_subtheme_process_page(&$variables) {
+    if (!isset($variables['node'])) {
+        return;
+    }
+    
+    if ($variables['node']->type != 'page') {
+        return;
+    }
+    
+    if($variables['node']->field_hide_title['und'][0]['value']) {
+      $variables['title'] = NULL;         
+    }
+}
