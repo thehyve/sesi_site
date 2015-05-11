2c5a795d50340693283b6c9ddb648808ff3efa78
dwerbam <dwerbam@gmail.com>
2015-05-11 14:26:13 +0000

    USESI-306 Login screen is returning to the originated destination

---
+++ patch/mica_distribution/modules/logintoboggan/logintoboggan.unifiedlogin.js
@@ -0,0 +1,36 @@
+
+(function ($) {
+
+Drupal.behaviors.unifiedLogin = {
+
+  attach: function (context) {
+    // Attach behaviors to the links so that they show/hide forms appropriately.
+    $('.toboggan-unified #register-link').click(function() {
+      $(this).addClass('btn-primary').blur();
+      $('.toboggan-unified #login-link').removeClass('btn-primary');
+      $('.toboggan-unified #register-form').show();
+      $('.toboggan-unified #login-form').hide();
+      return false;
+    });
+    $('.toboggan-unified #login-link').click(function() {
+      $(this).addClass('btn-primary').blur();
+      $('.toboggan-unified #register-link').removeClass('btn-primary');
+      $('.toboggan-unified #login-form').show();
+      $('.toboggan-unified #register-form').hide();
+      return false;
+    });
+
+    switch(Drupal.settings.LoginToboggan.unifiedLoginActiveForm) {
+      case 'register':
+        $('.toboggan-unified #register-link').click();
+        break;
+      case 'login':
+      default:
+        $('.toboggan-unified #login-link').click();
+        break;
+    }
+  }
+};
+
+})(jQuery);
+
