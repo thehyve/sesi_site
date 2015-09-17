627af13d7d56abce182bf5d34a26f460c4985f96
root <root@localhost.localdomain>
2015-09-17 18:37:52 +0000

    User menu fixed length setup by 79px

--- patch/mica_distribution/themes/mica_bootstrap/template.php
+++ patch/mica_distribution/themes/mica_bootstrap/template.php
@@ -13,7 +13,7 @@ function mica_bootstrap_bootstrap_based_theme() {
 function mica_bootstrap_menu_tree__user_menu($variables) {
   global $user;
   return '<div id="user-menu" class="pull-right btn-group" >'
-  . '<a style="width:161px;" class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user"></i> ' . htmlspecialchars($user->name) . ' <span class="caret"></span></a>'
+  . '<a style="width:79px;" class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user"></i> ' . htmlspecialchars($user->name) . ' <span class="caret"></span></a>'
   . '<ul class="dropdown-menu">' . $variables['tree'] . '</ul></div>';
 }
 
